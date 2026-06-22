e626904530a2f70acbdff027ccd73a12f32e1e9c
jash2810 <jashdetroj@gmail.com>
2026-06-22 10:26:22 -0400

    changes made

--- src/constants/hero.constants.js
+++ src/constants/hero.constants.js
@@ -4,7 +4,7 @@ export const HERO = {
   highlights: [
     { label: "Experience", value: "4+ yrs" },
     { label: "Focus", value: "AppSec & Vuln Mgmt" },
-    { label: "App Sec", valie: "SAST/DAST/IAST/SCA, OWASP 10"},
+    { label: "App Sec", value: "SAST/DAST/IAST/SCA, OWASP 10"},
     { label: "Artificial Intelligence Security", value: "LLM/AI Security, OWASP LLM 10"},
     { label: "Automation", value: "Python | AI" },
     { label: "Software Development", value: "JavaScript & Java"}
