2815d01f2f2df7a59c23bb35d087bfff4b7b98ce
Jash Patel <37921859+jash2810@users.noreply.github.com>
2026-06-22 10:45:12 -0400

    Update email address in site constants

--- src/constants/site.constants.js
+++ src/constants/site.constants.js
@@ -3,7 +3,7 @@ export const SITE = {
   headline:
     "IT Security Analyst | Full-Stack Developer | Bridging Development & Cybersecurity for Robust Applications",
   location: "Toronto, ON, Canada",
-  email: "jashpatel.work@gmail.com",
+  email: "jashpatel.works@gmail.com",
   linkedinUrl: "https://www.linkedin.com/in/jashdetroj/",
   githubUrl: "https://github.com/jash2810/",
   phone: "+1 (905) 922-2351",
